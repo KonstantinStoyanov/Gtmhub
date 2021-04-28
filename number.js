ae43b37dfc57bc3e0ddb466a5119d85d58105de8
KonstantinStoyanov <76064841+KonstantinStoyanov@users.noreply.github.com>
2021-04-28 12:01:11 +0300

    -error correction

--- number.js
+++ number.js
@@ -2,9 +2,12 @@ function number(num1,num2){
     let counter=0;
     let newNumber;
     newNumber=num1%10+num2%10;
-    while(newNumber>9){
+    while(newNumber>=9 || newNumber==0){
         if(newNumber>9){
             counter++
+        }
+        if(num1==0 && num2==0 ){
+            return console.log(counter);
         }
         num1=Math.floor(num1/10);
         num2=Math.floor(num2/10);
@@ -21,3 +24,4 @@ number(9,11);
 number(1,999);
 number(1,101);
 number(25,185)
+number(990,110)
